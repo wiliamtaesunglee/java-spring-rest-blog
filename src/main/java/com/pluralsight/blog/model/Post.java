1f6c21f27be35e22d6e19dd2f2b1e1a7fdaef49d
wiliam lee <wiliam.tae@gmail.com>
2021-12-19 07:53:08 -0300

    add title annotation

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -17,6 +17,8 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min = 4, max = 100)
     private String title;
     @Column(length=1000000)
     @Lob
